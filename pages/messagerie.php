46eb3f7f08b25be097ce6dca4e483d21daf56b30
David <d.carneiro@ecole-ipssi.net>
2022-03-21 14:26:57 +0100

    projet avance david

--- pages/messagerie.php
+++ pages/messagerie.php
@@ -112,7 +112,6 @@ if(!empty($_SESSION["statut"]) && $_SESSION["statut"] == "Etudiant" || $_SESSION
                                         <a class="btn btn-success" href="conversation.php?idReceveur=<?=$conversation["idEnvoyeur"];?>&idConversation=<?=$conversation["idConversation"];?>">Afficher la conversation</a>
                                         <?php
                                     }
-                                    
                                 ?>
                             </div>
                         </div>
